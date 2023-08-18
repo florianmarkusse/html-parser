61e4308b0a90f811989203f1a8c463372889d666
florianmarkusse <florianmarkusse@hotmail.com>
2023-08-18 21:25:37 +0200

    update cmkaelists

--- build-run.sh
+++ build-run.sh
@@ -7,7 +7,8 @@ BOLD='\033[1m'
 NO_COLOR='\033[0m'
 
 # Default values
-BUILD_MODE="release"
+BUILD_MODE="Release"
+BUILD_SHARED_LIBS=false
 RUN_TESTS=false
 RUN_BENCHMARKS=false
 # Initialize the positional arguments
@@ -17,7 +18,7 @@ EXECUTABLE=""
 function display_usage() {
 	echo -e "${RED}${BOLD}Usage: $0 <${YELLOW}EXECUTABLE${RED}> [${YELLOW}OPTIONS${RED}]${NO_COLOR}"
 	echo -e "${BOLD}Options:${NO_COLOR}"
-	echo -e "  -m, --build-mode <TYPE>    Set the build mode (${YELLOW}debug${NO_COLOR} or ${YELLOW}release${NO_COLOR}). Default is ${YELLOW}release${NO_COLOR}."
+	echo -e "  -m, --build-mode <TYPE>    Set the build mode (${YELLOW}Debug${NO_COLOR} or ${YELLOW}Release${NO_COLOR}). Default is ${YELLOW}Release${NO_COLOR}."
 	echo -e "  -t, --run-tests            Run tests after building."
 	echo -e "  -b, --run-benchmarks       Run benchmarks after building."
 	echo -e "  -h, --help                 Display this help message."
@@ -28,8 +29,8 @@ function display_usage() {
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
 	-m | --build-mode)
-		if [[ "$2" != "debug" && "$2" != "release" ]]; then
-			echo -e "${RED}${BOLD}Invalid ${YELLOW}BUILD_MODE${RED}. Valid options: ${NO_COLOR}${YELLOW}debug${BOLD}${RED}, ${NO_COLOR}${YELLOW}release${BOLD}${RED}.${NO_COLOR}"
+		if [[ "$2" != "Debug" && "$2" != "Release" ]]; then
+			echo -e "${RED}${BOLD}Invalid ${YELLOW}BUILD_MODE${RED}. Valid options: ${NO_COLOR}${YELLOW}Debug${BOLD}${RED}, ${NO_COLOR}${YELLOW}Release${BOLD}${RED}.${NO_COLOR}"
 			exit 1
 		fi
 		BUILD_MODE="$2"
@@ -46,6 +47,10 @@ while [[ "$#" -gt 0 ]]; do
 	-h | --help)
 		display_usage
 		;;
+	-s | --shared-libs)
+		BUILD_SHARED_LIBS="ON"
+		shift
+		;;
 	*)
 		EXECUTABLE="$1"
 		shift
@@ -60,15 +65,16 @@ if [[ -z "$EXECUTABLE" ]]; then
 fi
 
 # Check if the provided BUILD_MODE is valid (this check is already done in the loop, but we repeat it here for clarity)
-if [[ "$BUILD_MODE" != "debug" && "$BUILD_MODE" != "release" ]]; then
-	echo -e "${RED}${BOLD}Invalid ${YELLOW}BUILD_MODE${RED}. Valid options: ${NO_COLOR}${YELLOW}debug${BOLD}${RED}, ${NO_COLOR}${YELLOW}release${BOLD}${RED}.${NO_COLOR}"
+if [[ "$BUILD_MODE" != "Debug" && "$BUILD_MODE" != "Release" ]]; then
+	echo -e "${RED}${BOLD}Invalid ${YELLOW}BUILD_MODE${RED}. Valid options: ${NO_COLOR}${YELLOW}Debug${BOLD}${RED}, ${NO_COLOR}${YELLOW}Release${BOLD}${RED}.${NO_COLOR}"
 	exit 1
 fi
 
 # Display the configuration
 echo -e "${BOLD}${YELLOW}Configuration...${NO_COLOR}"
-echo -e "${BOLD}${YELLOW}EXECUTABLE${NO_COLOR}: ${YELLOW}$EXECUTABLE${NO_COLOR}"
-echo -e "${BOLD}${YELLOW}BUILD_MODE${NO_COLOR}: ${YELLOW}$BUILD_MODE${NO_COLOR}"
+echo -e "${BOLD}${YELLOW}EXECUTABLE${NO_COLOR}: ${YELLOW}${EXECUTABLE}${NO_COLOR}"
+echo -e "${BOLD}${YELLOW}BUILD_MODE${NO_COLOR}: ${YELLOW}${BUILD_MODE}${NO_COLOR}"
+echo -e "${BOLD}${YELLOW}SHARED LIBS:${NO_COLOR}: ${YELLOW}${BUILD_SHARED_LIBS}${NO_COLOR}"
 if [ "$RUN_TESTS" = true ]; then
 	echo -e "${BOLD}${YELLOW}Run tests${NO_COLOR}: ${YELLOW}Yes${NO_COLOR}"
 else
@@ -82,7 +88,7 @@ fi
 echo ""
 
 # Perform the build based on the options
-cmake -S . -B build/ -D CMAKE_BUILD_TYPE="$BUILD_MODE" -D RUN_TESTS="$RUN_TESTS" -D RUN_BENCHMARKS="$RUN_BENCHMARKS"
+cmake -S . -B build/ -D CMAKE_BUILD_TYPE="$BUILD_MODE" -D BUILD_SHARED_LIBS="$BUILD_SHARED_LIBS" -D BUILD_TESTS="$RUN_TESTS" -D BUILD_BENCHMARKS="$RUN_BENCHMARKS"
 cmake --build build/
 
 if [ "$RUN_TESTS" = true ]; then
